23b0e9a0fd0f38ec26e25d94e15952b1ba7c64e3
gentij <gentijashari5@gmail.com>
2023-05-18 20:11:38 +0200

    feat: env variables config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
 export const APP_NAME = 'FelidaeDAO';
 export const CUSTOM_RPC_METHODS = {};
-export const PROVIDER_SOCKET = 'wss://panther-testnet.live';
+export const PROVIDER_SOCKET = process.env.NEXT_PUBLIC_PROVIDER_SOCKET;
 
 export const LOCAL_STORAGE_ADDRESS_KEY = 'address';
+
+export const CONTRACT_CID = process.env.NEXT_PUBLIC_CONTRACT_CID;
