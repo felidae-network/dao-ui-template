ec28a9016878e75ee870fbacbe820e3ef78017b5
gentij <gentijashari5@gmail.com>
2023-05-25 20:58:50 +0200

    feat: infer arguments types from schemas

--- src/types/schemaTypes/index.ts
+++ src/types/schemaTypes/index.ts
@@ -1,5 +1,61 @@
 import { InferType } from 'yup';
 
-import { addMemberInputSchema } from '@/helpers/schemas';
+import {
+  addDaoAsMemberInputSchema,
+  addDaoTokenInputSchema,
+  addMemberInputSchema,
+  calculateEfficiencyInputSchema,
+  closeProjectInputSchema,
+  closeTaskInputSchema,
+  createProjectInputSchema,
+  createTicketInputSchema,
+  deleteMemberInputSchema,
+  getMemberInfoInputSchema,
+  getMemberListInputSchema,
+  getProjectInputSchema,
+  getStakeForAccountInputSchema,
+  getTaskInfoInputSchema,
+  getTokenListInputSchema,
+  isAdminInputSchema,
+  isMemberInputSchema,
+  setCodeInputSchema,
+  setDaoAdminInputSchema,
+  timeLogInputSchema,
+  updateMemberRoleInputSchema,
+  updateProjectStatusInputSchema,
+  updateTaskStatusInputSchema,
+} from '@/helpers/schemas';
 
 export type ADdMemberInput = InferType<typeof addMemberInputSchema>;
+export type AddDaoTokenInput = InferType<typeof addDaoTokenInputSchema>;
+export type GetTokenListInput = InferType<typeof getTokenListInputSchema>;
+export type IsAdminInput = InferType<typeof isAdminInputSchema>;
+export type DeleteMemberInput = InferType<typeof deleteMemberInputSchema>;
+export type AddDaoAsMemberInput = InferType<typeof addDaoAsMemberInputSchema>;
+export type GetMemberInfoInput = InferType<typeof getMemberInfoInputSchema>;
+export type SetDaoAdminInput = InferType<typeof setDaoAdminInputSchema>;
+export type IsMemberInput = InferType<typeof isMemberInputSchema>;
+export type GetMemberListInput = InferType<typeof getMemberListInputSchema>;
+export type UpdateMemberRoleInput = InferType<
+  typeof updateMemberRoleInputSchema
+>;
+export type CreateProjectInput = InferType<typeof createProjectInputSchema>;
+export type GetProjectInput = InferType<typeof getProjectInputSchema>;
+export type UpdateProjectStatusInput = InferType<
+  typeof updateProjectStatusInputSchema
+>;
+export type CloseProjectInput = InferType<typeof closeProjectInputSchema>;
+export type CreateTicketInput = InferType<typeof createTicketInputSchema>;
+export type UpdateTaskStatusInput = InferType<
+  typeof updateTaskStatusInputSchema
+>;
+export type GetTaskInfoInput = InferType<typeof getTaskInfoInputSchema>;
+export type CloseTaskInput = InferType<typeof closeTaskInputSchema>;
+export type TimeLogInput = InferType<typeof timeLogInputSchema>;
+export type CalculateEfficiencyInput = InferType<
+  typeof calculateEfficiencyInputSchema
+>;
+export type GetStakeForAccountInput = InferType<
+  typeof getStakeForAccountInputSchema
+>;
+export type SetCodeInput = InferType<typeof setCodeInputSchema>;
