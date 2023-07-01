bbd90dd3232b09be940b4ad0211c6098fa340f23
gentij <gentijashari5@gmail.com>
2023-07-01 21:28:40 +0200

    feat: seperate log accordion to its own component

--- src/components/tickets/TicketBoard/components/TicketModal.tsx
+++ src/components/tickets/TicketBoard/components/TicketModal.tsx
@@ -1,19 +1,9 @@
-import { FormEvent, useEffect, useState } from 'react';
-import {
-  Badge,
-  Button,
-  Collapse,
-  Divider,
-  Modal,
-  Progress,
-  Select,
-} from 'react-daisyui';
-import toast from 'react-hot-toast';
+import { Badge, Button, Divider, Modal, Progress, Select } from 'react-daisyui';
 import { AiOutlineClockCircle, AiOutlineClose } from 'react-icons/ai';
-import { AiOutlinePlus } from 'react-icons/ai';
 
-import { useTimeLog } from '@/hooks/messages';
 import { IGetTicket } from '@/hooks/messages/useGetTicketList';
+
+import { TimeLogAccordion } from '@/components/tickets/TicketBoard/components/TimeLogAccordion';
 
 import { useSubstrateState } from '@/context/substrate/SubstrateContextProvider';
 
@@ -29,29 +19,6 @@ export const TicketModal: React.FC<TicketModalProps> = ({
   closeModal,
 }) => {
   const { currentAccount } = useSubstrateState();
-  const { mutate, setArgValues, loading } = useTimeLog({
-    taskId: Number(ticket.ticketId),
-    time: 0,
-  });
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [collapse, setCollapse] = useState(false);
-
-  useEffect(() => {
-    setArgValues((prevState) => ({ ...prevState, time: hours * 60 + minutes }));
-  }, [hours, minutes, setArgValues]);
-
-  const handleLogTime = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const mutateValue = await mutate();
-    if (mutateValue) {
-      if (mutateValue.isError)
-        return toast.error(mutateValue.decodedOutput || 'Something went wrong');
-
-      toast.success('Time logged');
-    }
-  };
 
   return (
     <>
@@ -130,67 +97,7 @@ export const TicketModal: React.FC<TicketModalProps> = ({
               </div>
 
               {currentAccount.address === ticket.assignedTo && (
-                <Collapse open={collapse} checkbox icon='arrow'>
-                  <Collapse.Title
-                    onClick={() => setCollapse(!collapse)}
-                    className='bg-base-300 my-3 mb-0 cursor-pointer rounded rounded-b-none'
-                  >
-                    LOG TIME
-                  </Collapse.Title>
-                  <Collapse.Content className='bg-base-200 rounded rounded-t-none'>
-                    <form onSubmit={handleLogTime}>
-                      <div className='flex w-full gap-2'>
-                        <div className='w-1/2'>
-                          <label className='label'>
-                            <span className='label-text'>HOURS</span>
-                          </label>
-                          <Select
-                            color='info'
-                            placeholder='Ticket Type'
-                            className='w-full'
-                            defaultValue={0}
-                            value={hours}
-                            onChange={(e) => setHours(Number(e.target.value))}
-                          >
-                            {[...new Array(25)].map((_, index) => (
-                              <option value={index} key={index}>
-                                {index}
-                              </option>
-                            ))}
-                          </Select>
-                        </div>
-
-                        <div className='w-1/2'>
-                          <label className='label'>
-                            <span className='label-text'>MINUTES</span>
-                          </label>
-                          <Select
-                            color='info'
-                            placeholder='Ticket Type'
-                            className='w-full'
-                            defaultValue={0}
-                            value={minutes}
-                            onChange={(e) => setMinutes(Number(e.target.value))}
-                          >
-                            {[...new Array(61)].map((_, index) => (
-                              <option value={index} key={index}>
-                                {index}
-                              </option>
-                            ))}
-                          </Select>
-                        </div>
-                      </div>
-                      <Button
-                        className='mt-3 w-full'
-                        type='submit'
-                        startIcon={<AiOutlinePlus />}
-                        loading={loading}
-                      >
-                        LOG
-                      </Button>
-                    </form>
-                  </Collapse.Content>
-                </Collapse>
+                <TimeLogAccordion ticket={ticket} />
               )}
             </div>
           </div>
