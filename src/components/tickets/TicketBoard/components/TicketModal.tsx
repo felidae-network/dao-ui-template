dff4b4f820f9944b46cc122be666826b786c1fdc
gentij <gentijashari5@gmail.com>
2023-07-01 21:11:25 +0200

    feat: time log

--- src/components/tickets/TicketBoard/components/TicketModal.tsx
+++ src/components/tickets/TicketBoard/components/TicketModal.tsx
@@ -1,6 +1,18 @@
-import { Badge, Button, Divider, Modal, Progress, Select } from 'react-daisyui';
+import { FormEvent, useEffect, useState } from 'react';
+import {
+  Badge,
+  Button,
+  Collapse,
+  Divider,
+  Modal,
+  Progress,
+  Select,
+} from 'react-daisyui';
+import toast from 'react-hot-toast';
 import { AiOutlineClockCircle, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlinePlus } from 'react-icons/ai';
 
+import { useTimeLog } from '@/hooks/messages';
 import { IGetTicket } from '@/hooks/messages/useGetTicketList';
 
 import { TaskStatusEnum } from '@/types/enums/taskStatus.enum';
@@ -14,6 +26,30 @@ export const TicketModal: React.FC<TicketModalProps> = ({
   ticket,
   closeModal,
 }) => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [collapse, setCollapse] = useState(false);
+  const { mutate, setArgValues, loading } = useTimeLog({
+    taskId: Number(ticket.ticketId),
+    time: 0,
+  });
+
+  useEffect(() => {
+    setArgValues((prevState) => ({ ...prevState, time: hours * 60 + minutes }));
+  }, [hours, minutes, setArgValues]);
+
+  const handleLogTime = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const mutateValue = await mutate();
+    if (mutateValue) {
+      if (mutateValue.isError)
+        return toast.error(mutateValue.decodedOutput || 'Something went wrong');
+
+      toast.success('Time logged');
+    }
+  };
+
   return (
     <>
       <Modal.Header>
@@ -68,7 +104,7 @@ export const TicketModal: React.FC<TicketModalProps> = ({
           <div className='w-1/3'>
             <div>
               <label className='label'>
-                <span className='label-text'>STATUS</span>
+                <span className='label-text font-bold'>STATUS</span>
               </label>
               <Select color='info' placeholder='Ticket Type' className='w-full'>
                 {Object.keys(TaskStatusEnum).map((ticketType) => (
@@ -80,7 +116,7 @@ export const TicketModal: React.FC<TicketModalProps> = ({
             </div>
             <div className='mt-10'>
               <label className='label'>
-                <span className='label-text'>TIME TRACKING</span>
+                <span className='label-text font-bold'>TIME TRACKING</span>
               </label>
               <div className='flex items-center'>
                 <AiOutlineClockCircle className='mr-2 text-4xl' />
@@ -89,6 +125,67 @@ export const TicketModal: React.FC<TicketModalProps> = ({
                   <p className='text-sm opacity-75'>4d 3h 30m logged in</p>
                 </div>
               </div>
+              <Collapse open={collapse} checkbox icon='arrow'>
+                <Collapse.Title
+                  onClick={() => setCollapse(!collapse)}
+                  className='bg-base-300 my-3 mb-0 cursor-pointer rounded rounded-b-none'
+                >
+                  LOG TIME
+                </Collapse.Title>
+                <Collapse.Content className='bg-base-200 rounded rounded-t-none'>
+                  <form onSubmit={handleLogTime}>
+                    <div className='flex w-full gap-2'>
+                      <div className='w-1/2'>
+                        <label className='label'>
+                          <span className='label-text'>HOURS</span>
+                        </label>
+                        <Select
+                          color='info'
+                          placeholder='Ticket Type'
+                          className='w-full'
+                          defaultValue={0}
+                          value={hours}
+                          onChange={(e) => setHours(Number(e.target.value))}
+                        >
+                          {[...new Array(25)].map((_, index) => (
+                            <option value={index} key={index}>
+                              {index}
+                            </option>
+                          ))}
+                        </Select>
+                      </div>
+
+                      <div className='w-1/2'>
+                        <label className='label'>
+                          <span className='label-text'>MINUTES</span>
+                        </label>
+                        <Select
+                          color='info'
+                          placeholder='Ticket Type'
+                          className='w-full'
+                          defaultValue={0}
+                          value={minutes}
+                          onChange={(e) => setMinutes(Number(e.target.value))}
+                        >
+                          {[...new Array(61)].map((_, index) => (
+                            <option value={index} key={index}>
+                              {index}
+                            </option>
+                          ))}
+                        </Select>
+                      </div>
+                    </div>
+                    <Button
+                      className='mt-3 w-full'
+                      type='submit'
+                      startIcon={<AiOutlinePlus />}
+                      loading={loading}
+                    >
+                      LOG
+                    </Button>
+                  </form>
+                </Collapse.Content>
+              </Collapse>
             </div>
           </div>
         </div>
