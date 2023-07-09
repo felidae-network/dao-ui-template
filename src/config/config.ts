71e9ff7c4ce046c1bf6869b811fa5f1cba2cbdc6
gentij <gentijashari5@gmail.com>
2023-07-09 12:52:11 +0200

    fix: config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,10 +5,6 @@ export const PROVIDER_SOCKET = 'wss://panther-testnet.live';
 
 export const LOCAL_STORAGE_ADDRESS_KEY = 'address';
 
-export const CONTRACT_CID = 'QmQtVLC7kFt6mgbSGcKjrvzN2kR6PsD6kmjEWQAFjeMuj6';
-process.env.NEXT_PUBLIC_CONTRACT_CID;
-export const CONTRACT_ADDRESS =
-  '5HjDNwroKPdCAM3S1gCZkLJkWGY9zHXURz7WEgdS63pGq3VV';
-process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
-export const PUBLIISH_URL = 'https://publiish.io';
-// process.env.NEXT_PUBLIC_PUBLIISH_URL;
+export const CONTRACT_CID = process.env.NEXT_PUBLIC_CONTRACT_CID;
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+export const PUBLIISH_URL = process.env.NEXT_PUBLIC_PUBLIISH_URL;
